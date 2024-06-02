597bf9354ce9326ab8adfcb75fcc039d37517325
Shunsuke Tadokoro <s.tadokoro0317@gmail.com>
2024-06-02 17:52:37 +0900

    refine

--- routes/protected/index.tsx
+++ routes/protected/index.tsx
@@ -1,6 +1,11 @@
-import { define } from "../../utils.ts";
+import { FreshContext } from "https://jsr.io/@fresh/core/2.0.0-alpha.11/src/context.ts";
+import { AppState, define } from "../../utils.ts";
 
-export default define.page((ctx) => {
+export default async function Page(ctx: FreshContext<unknown, AppState>) {
+  const { value: joke } = await fetch(
+    "https://api.chucknorris.io/jokes/random",
+  ).then((res) => res.json());
+
   const memberId = ctx.state.authn.memberId;
   const scope = ctx.state.scope;
   const show = scope === "full"
@@ -8,10 +13,13 @@ export default define.page((ctx) => {
     : Array.from(scope).join(", ");
   return (
     <>
-      <h1>Protected Page</h1>
-      <p>This is a protected page.</p>
-      <p>Member ID: {memberId}</p>
-      <p>Scope: {show}</p>
+      <div class="joke">
+        <div class="title">
+          <h1>Wisdom of Chuck Norris</h1>
+          <p>You: {memberId} ({show})</p>
+        </div>
+        <cite>{joke}</cite>
+      </div>
     </>
   );
-});
+}
